6ebc2eea8a069ea8f58f2bc9d7376497c72a58ad
Dominic Ferrante <dominicferrante@vlan-194-195.onu.edu>
2020-11-19 19:00:10 -0500

    Finished ItemInfo and messed around with equipment page

--- src/components/ItemInfo.vue
+++ src/components/ItemInfo.vue
@@ -1,18 +1,29 @@
 <template>
     <div id="something">
-        <a-card>
-            <a-row type="flex" justify="center">
+        <a-card :style="{height: '95vh'}">
+            <a-row type="flex" justify="start">
                 <a-col :flex="1">
-                    <img id="logo" src="../assets/logo.png"/>
+                    <img id="logo" alt="Equipment Image" src="../assets/logo.png"/>
                 </a-col>
-                <a-col :flex="10" >
+                <a-col :flex="10" style="{textAlign: left;}">
                     <h1> <strong> Item Name </strong> </h1>
                     <p> This is an Item description </p>
                 </a-col>
-
+                <a-col :flex="1">
+                    <a-button type="primary" @click="changeDrawerVisible"> Equipment </a-button>
+                </a-col>
             </a-row>
-            <a-divider></a-divider>
+            <!--<a-divider orientation="left"> Current Tab </a-divider>-->
+            
+            <!-- I think content of tabs are lazy loaded so we shouldn't run into a performance hit? -->
+            <a-tabs type="card" tab-position="top">
+                <a-tab-pane key="1" tab="Reports"> Reports Here </a-tab-pane>
+                <a-tab-pane key="2" tab="Parts"> Parts Here </a-tab-pane>
+                <a-tab-pane key="3" tab="Schedule"> Schedule Here </a-tab-pane>
+                <a-tab-pane key="4" tab="Docs"> Documents Here </a-tab-pane>
+            </a-tabs>
         </a-card>
+        
     </div>
 </template>
 
@@ -23,7 +34,7 @@ export default {
 </script>
 
 <style scoped>
-#logo{
+img#logo{
     max-height: 100px;
     max-width: 100px;
 }
--- src/views/Equipment.vue
+++ src/views/Equipment.vue
@@ -1,8 +1,5 @@
 <template>
     <div>
-        <h1>
-            This is the Equipment Page
-        </h1>
         <ItemInfo/>
         <Drawer />
     </div>
@@ -11,13 +8,37 @@
 <script>
 import Drawer from '@/components/Drawer.vue';
 import ItemInfo from '@/components/ItemInfo.vue';
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        drawerVisible: false,
+    },
+    mutations: {
+        changeDrawerVisible: () => {
+            return this.drawerVisible ? this.drawerVisible = false : this.drawerVisible = true;
+        }
+    }
+});
 
 export default {
     name: 'Equipment',
-
     components: {
         Drawer,
         ItemInfo
+    },
+    methods: {
+        flip(){
+            store.commit('changeDrawerVisible');
+        }
+    },
+    computed: {
+        drawerVisible(){
+            return store.state.drawerVisible;
+        }
     }
 }
 </script>
