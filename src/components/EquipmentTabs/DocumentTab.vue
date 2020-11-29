89b512b489532075571dc643b8163266df2a6061
Dominic Ferrante <dominicferrante@iMac-Pro.local>
2020-11-29 17:43:28 -0500

    added some things to document tabs

--- src/components/EquipmentTabs/DocumentTab.vue
+++ src/components/EquipmentTabs/DocumentTab.vue
@@ -1,43 +1,52 @@
 <template>
-    <a-table :columns="columns" :data-source="data" bordered>
-        <template v-for="col in ['document']" :slot="col" slot-scope="text, record">
-            <div :key="col">
-                <a-input v-if="record.editable" style="margin: -5px 0" :value="text" @change="e => handleChange(e.target.value, record.key, col)" />
-                <template v-else>
-                    {{ text }}
-                </template>
-            </div>
-        </template>
-        <span slot="tags" slot-scope="tags, record">
-            <a-tag v-for="tag in tags" :key="tag" :color="tag === 'helpful' ? 'green' : 'blue'">
-                {{tag.toLowerCase()}}
-            </a-tag>
-            <template v-if="record.editable">
-                <a @click="addTag">
-                    <a-icon type="plus-square" />
-                </a> 
-            </template>
-        </span>
-        <template slot="action" slot-scope="text, record">
-            <div class="editable-row-operations">
-                <span v-if="record.editable">
-                    <a-popconfirm title="Sure to cancel?" @confirm="() => cancel(record.key)">
-                        <a>
-                            Cancel
+    <div>
+        <div id="buttonHolder" :style="{paddingBottom: '10px'}">
+            <a-button type="primary"> 
+                Add Document 
+            </a-button>
+        </div>
+        <a-table :columns="columns" :data-source="data">
+            <template v-for="col in ['document']" :slot="col" slot-scope="text, record">
+                <div :key="col">
+                    <a-input v-if="record.editable" style="margin: -5px 0" :value="text" @change="e => handleChange(e.target.value, record.key, col)" />
+                    <template v-else>
+                        <a :href="record.href"> 
+                            {{ text }} 
                         </a>
-                    </a-popconfirm>
-                    <a @click="() => save(record.key)">
-                        Save
-                    </a>
-                </span>
-                <span v-else>
-                    <a :disabled="editingKey !== ''" @click="() => edit(record.key)">
-                        Edit
-                    </a>
-                </span>
-            </div>
-        </template>
-    </a-table>
+                    </template>
+                </div>
+            </template>
+            <span slot="tags" slot-scope="tags, record">
+                <a-tag v-for="tag in tags" :key="tag" :color="tag === 'helpful' ? 'green' : 'blue'">
+                    {{tag.toLowerCase()}}
+                </a-tag>
+                <template v-if="record.editable">
+                    <a @click="addTag">
+                        <a-icon type="plus-square" />
+                    </a> 
+                </template>
+            </span>
+            <template slot="action" slot-scope="text, record">
+                <div class="editable-row-operations">
+                    <span v-if="record.editable">
+                        <a-popconfirm title="Sure to cancel?" @confirm="() => cancel(record.key)">
+                            <a>
+                                Cancel
+                            </a>
+                        </a-popconfirm>
+                        <a @click="() => save(record.key)">
+                            Save
+                        </a>
+                    </span>
+                    <span v-else>
+                        <a :disabled="editingKey !== ''" @click="() => edit(record.key)">
+                            Edit
+                        </a>
+                    </span>
+                </div>
+            </template>
+        </a-table>
+    </div>
 </template>
 
 <script>
