99a02340e5b170425e36ba6ef2079853d74e970c
ONU <onu@vlan-94-152.onu.edu>
2020-12-01 11:14:14 -0500

    Multi Map using Store

--- src/components/liveMapComponents/MapImg.vue
+++ src/components/liveMapComponents/MapImg.vue
@@ -1,25 +1,24 @@
 <template>
       <v-image :config="{
-            image: image,
-            width:900,
-            height:900
+            image: mapImage, 
+            width: 900,
+            height: 900
           }"/>
 </template>
 
 <script>
-import store from "../../store/index.js"
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
 let items= [
-      {path: '../../assets/liveMapAssets/A701-1.png'},
-      {path: '../../assets/liveMapAssets/A801-1.png'},
-      {path: '../../assets/liveMapAssets/A802-1.png'},
-      {path: '../../assets/liveMapAssets/A803-1.png'},
-      {path: '../../assets/liveMapAssets/A804-1.png'},
-      {path: '../../assets/liveMapAssets/A805-1.png'},
-      {path: '../../assets/liveMapAssets/A806-1.png'}
+      {path: require('../../assets/liveMapAssets/A701-1.png')},
+      {path: require('../../assets/liveMapAssets/A801-1.png')},
+      {path: require('../../assets/liveMapAssets/A802-1.png')},
+      {path: require('../../assets/liveMapAssets/A803-1.png')},
+      {path: require('../../assets/liveMapAssets/A804-1.png')},
+      {path: require('../../assets/liveMapAssets/A805-1.png')},
+      {path: require('../../assets/liveMapAssets/A806-1.png')}
     ]
 
 export default {
@@ -29,22 +28,24 @@ export default {
       stageSize: {
         width: width,
         height: height
-      },
-      image: null
+      }
     };
   },
   created() {
-    const image = new window.Image();
-    //let item = items[this.AreaNo];
-    //image.src = require(items[getCurrentMap()]); // replace with item.path to revert
-    image.onload = () => {
-      // set image only when it is loaded
-      this.image = image;
-    };
+    
   },
   computed: {
-    getCurrentMap(){
-    return store.getters.getMapState;
+    path(){
+      return items[this.$store.getters.getMapState].path;
+    },
+    mapImage(){
+      const image = new window.Image();
+      image.src = this.path;
+      //image.onload = () => {
+        // set image only when it is loaded
+        //this.image = image;
+      //};
+      return image;
     }
   }
 };
