437b3c48000a9d2cbcb4212756b25da01db23e3e
Wayne Geissinger <w-geissinger@onu.edu>
2020-12-03 17:32:35 -0500

    code removal

--- src/main.js
+++ src/main.js
@@ -7,25 +7,6 @@ import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 import VueKonva from 'vue-konva'
 
-/*
-const CSVtoJSON = require("csvtojson");
-const JSONtoCSV = require("json2csv").parse;
-const FileSystem = require("fs");
-
-//CSV to JSON push and conversion
-CSVtoJSON().fromFile("./sourcefile.csv").then(source => {
-  console.log(source);
-  source.push({
-    //whatever columns we have in the CSV
-    //format
-    //"column": "value"
-  });
-  //Converstion back to CSV
-  const csv = JSONtoCSV(source, {fields: [column, column, column]});
-  FileSystem.writeFileSync("./newcsvname.csv", csv);
-});
-*/
-
 Vue.config.productionTip = false
 
 Vue.use(VueKonva);
