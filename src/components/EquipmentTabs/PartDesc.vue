b8e74ecaa48c4161f841a791c01162dd6f5d584b
Dominic Ferrante <dominicferrante@Dominics-MacBook-Air.local>
2020-12-05 09:26:34 -0500

    Added PartDesc.vue to modularise the process of getting information from a part

---
+++ src/components/EquipmentTabs/PartDesc.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{curPart.desc}} <br>
+        Location: {{curPart.location}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PartDesc',
+    props: ['curPart']
+    
+}
+</script>
--- src/components/EquipmentTabs/PartTab.vue
+++ src/components/EquipmentTabs/PartTab.vue
@@ -3,7 +3,7 @@
         <a-list-item slot="renderItem" slot-scope="part">
             <a :href="part.href">
                 <a-card :src="part.href" :title="part.name" :hoverable="true">
-                    {{part.desc}}
+                    <PartDesc :curPart="part" />
                 </a-card>
             </a>
         </a-list-item>
@@ -12,20 +12,25 @@
 
 <script>
 
+import PartDesc from '@/components/EquipmentTabs/PartDesc.vue';
+
 const data = [
-    {name: "Part 1", desc: "Description of Part 1", href: 'https://www.antdv.com/'},
-    {name: "Part 2", desc: "Description of Part 2", href: 'https://www.antdv.com/'},
-    {name: "Part 3", desc: "Description of Part 3", href: 'https://www.antdv.com/'},
-    {name: "Part 4", desc: "Description of Part 4", href: 'https://www.antdv.com/'},
-    {name: "Part 5", desc: "Description of Part 5", href: 'https://www.antdv.com/'},
-    {name: "Part 6", desc: "Description of Part 6", href: 'https://www.antdv.com/'},
-    {name: "Part 7", desc: "Description of Part 7", href: 'https://www.antdv.com/'},
+    {name: "Part 1", desc: "Description of Part 1", location: "Room 1", href: 'https://www.antdv.com/'},
+    {name: "Part 2", desc: "Description of Part 2", location: "Room 2", href: 'https://www.antdv.com/'},
+    {name: "Part 3", desc: "Description of Part 3", location: "Room 3", href: 'https://www.antdv.com/'},
+    {name: "Part 4", desc: "Description of Part 4", location: "Room 4", href: 'https://www.antdv.com/'},
+    {name: "Part 5", desc: "Description of Part 5", location: "Room 5", href: 'https://www.antdv.com/'},
+    {name: "Part 6", desc: "Description of Part 6", location: "Room 6", href: 'https://www.antdv.com/'},
+    {name: "Part 7", desc: "Description of Part 7", location: "Room 7", href: 'https://www.antdv.com/'},
 ];
 
 export default {
     name: "PartTab",
     data (){
         return {data};
+    },
+    components: {
+        PartDesc
     }
 }
 </script>
